14e04183d4879d0dff6af3098badaf36e6a43946
Joshua <jbkunst@gmail.com>
2017-01-07 22:29:41 -0300

    1st try

---
+++ www/updatehc.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  
+  // var chart = $("#hc").highcharts();
+    
+  $("#valtoupdate").bind("DOMSubtreeModified",function(){
+    
+    var y = parseFloat($("#valtoupdate").text()); 
+    
+    console.log("changed: ", y);
+    
+    if(!isNaN(y)) {
+      
+      var x = (new Date()).getTime(); // current time
+      
+      console.log("updating: ", y, " on: ", x);
+      
+      $("#hc").highcharts().series[0].addPoint([x, y, true, true]);
+      
+    }
+  
+  });
+  
+});
